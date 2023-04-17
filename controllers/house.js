2f2c7ca7fa33747b63421e2f020f964c7e71cc30
fisherreese2R <s540308@nwmissouri.edu>
2023-04-17 14:09:30 -0500

    part 4 adjustment

--- controllers/house.js
+++ controllers/house.js
@@ -36,7 +36,7 @@ exports.house_view_one_Page = async function(req, res) {
     console.log("single view for id " + req.query.id)
     try{
     result = await house.findById( req.query.id)
-    res.render('costumedetail',
+    res.render('housedetail',
    { title: 'House Detail', toShow: result });
     }
     catch(err){
