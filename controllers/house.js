b98ebafc8e01f95eac1c4e5c41ea2e5c2bd27186
fisherreese2R <s540308@nwmissouri.edu>
2023-04-18 11:23:37 -0500

    part 7

--- controllers/house.js
+++ controllers/house.js
@@ -145,3 +145,17 @@ exports.house_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+
+   // Handle a delete one view with id from query
+exports.house_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await house.findById(req.query.id)
+    res.render('housedelete', { title: 'House Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/Houses.js
+++ routes/Houses.js
@@ -7,5 +7,9 @@ router.get('/', house_controlers.house_view_all_Page );
 router.get('/detail', house_controlers.house_view_one_Page);
 /* GET create House page */
 router.get('/create', house_controlers.house_create_Page);
+/* GET create update page */
+router.get('/update', house_controlers.house_update_Page);
+/* GET delete costume page */
+router.get('/delete', house_controlers.house_delete_Page);
 
 module.exports = router;
